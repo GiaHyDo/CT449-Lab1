3e2077c5e0d49fff51b4800d1cb03115d6eeba2c
GiaHyDo <hyb2016970@student.ctu.edu.vn>
2024-02-21 10:41:11 +0700

    Dinh nghia ca route cho tai nguyen Contact

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,27 @@
+exports.create = (req, res) => {
+    res.send({ message: "create handler" });
+};
+    
+exports.ƒindALL = (req, res) => {
+    res.send({ message: "findAll handler" });
+};
+
+exports.ƒindØne = (req, res) => {
+    res.send({ message: "findOne handler" });
+};
+    
+exports.update = (req, res) => {
+    res.send({message: "update handler" });
+};
+    
+exports.deLete = (req, res) => {
+    res.send({ message: "delete handler" });
+};
+    
+exports.deLeteALL = (req, res) => {
+    res.send({ message: "deleteAll handler" });
+};
+    
+exports.ƒinđALLFavorite = (req, res) => {
+    res.send({ message: "findAllFavorite handler" });
+};
